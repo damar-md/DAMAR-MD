ba8179f235f3798f21e5eb73bc0e41072a3e4866
oudaelmontaqim-bit <oudaelmontaqim@gmail.com>
2026-07-23 14:10:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212633226400;
+global.pairingNumber = 212633226499;
 global.owner = [
   ['212633226499', 'DAMAR-MD', true],
-  ['', 'Owner 2', true],
+  ['', 'Owner 10', true],
 ];
 
 global.namebot = 'gaff ai';
 global.author = 'DAMAR-MD';
-global.source = 'https://chat.whatsapp.com/Hp3R0WWD5G8Li9HKBU6fn3';
+global.source = 'https://www.facebook.com/profile.php?id=61591783185803';
 
 global.wait = 'Loading... | جاري الانتظار';
 global.eror = 'There is an error... | وقع خطأ';

eb46383d8a855422cd83bf8484385d6b7f4cc907
oudaelmontaqim-bit <oudaelmontaqim@gmail.com>
2026-07-24 18:35:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,10 @@ global.stickauth = namebot;
 
 global.multiplier = 38; // The higher, The harder levelup
 
-/*============== EMOJI ==============*/
+global.audioIntro = 'https://pdftolink.io/file/r2_dXNlcnMvZ3Vlc3QvZjhiNWYyMDItODU3YS00YTRmLTg2OGYtYTY4ZjAyOGI5ZmE0Lm00YQ' 
+global.audioMenu = 'https://pdftolink.io/file/r2_dXNlcnMvZ3Vlc3QvZjhiNWYyMDItODU3YS00YTRmLTg2OGYtYTY4ZjAyOGI5Lmm00YQ'
+
+YQ*============== EMOJI ==============*/
 global.rpg = {
 	emoticon(string) {
 		string = string.toLowerCase();

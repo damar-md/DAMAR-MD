b78524e3d244cfe329849c41b53416aa3b881af7
oudaelmontaqim-bit <oudaelmontaqim@gmail.com>
2026-07-25 23:58:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212633226499;
+global.pairingNumber = 212633226400;
 global.owner = [
   ['212633226499', 'DAMAR-MD', true],
   ['', 'Owner 10', true],
